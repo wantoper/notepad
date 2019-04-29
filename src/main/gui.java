ff6d036946528da1b6913caf7fd3eefecc98c1d1
Administrator <Administrator@PC-49>
2019-04-29 11:16:48 +0800

    半产品

--- src/main/gui.java
+++ src/main/gui.java
@@ -1,17 +1,21 @@
 package main;
 
 import java.awt.Dimension;
+import java.awt.Panel;
 import java.awt.TextArea;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
-import javax.swing.RootPaneContainer;
 
 public class gui {
-
+	static List<File> list = new ArrayList<>();
+	
 	public static void main(String[] args) {
 		JFrame j = new JFrame();
 		j.setTitle("test");
@@ -19,29 +23,56 @@ public class gui {
         j.setLocation(20, 10);
 		j.setLayout(null);
 		
-		JPanel p = new JPanel();
-		JTextArea ta = new JTextArea();
-        
-        //自动换行
-        ta.setPreferredSize(new Dimension(980, 580));
-        ta.setLineWrap(true);
-        JScrollPane sp = new JScrollPane(ta);
-        p.add(sp);
-        
+		filea(new File("D:/java"));
 		
-		JPanel p2 = new JPanel();
+		JTabbedPane jt = new JTabbedPane();
 		
-		JTabbedPane jt1 = new JTabbedPane();
 		
-		jt1.add(p);
-		jt1.add(p2);
+		for(int x =0;x<list.size();x++){
+			jt.add( newpane(list.get(x)));
+			jt.setTitleAt(x, list.get(x).getName());
+		}
 		
-		jt1.setTitleAt(0, "红色tab");
-		jt1.setTitleAt(1, "红色tab");
-		
-		j.setContentPane(jt1);
+		j.setContentPane(jt);
 		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		j.setVisible(true);
+	}
+
+    public static JPanel newpane(File file){
+    	JPanel p = new JPanel();
+    	JTextArea jt = new JTextArea();
+    	char[] all = new char[(int) file.length()];
+        try (FileReader input = new FileReader(file)) {
+            input.read(all);
+        } catch (Exception e) {
+            e.getMessage();
+        }
+        jt.setText(new String(all));
+        p.add(jt);
+		return p;
+    }
+	
+	
+	public static void filea(File file){
+		if(file.exists()){
+			
+			if(file.isFile()){
+				if (file.getName().endsWith(".java")) {
+	                list.add(file);
+	            }
+			}
+			if(file.isDirectory()){
+				File[] temp = file.listFiles();
+	            if (temp != null) {
+	                for (File file2 : temp) {
+	                    filea(file2);
+	                }
+	            }
+			}
+			
+		}else{
+			System.out.println("文件不存在");
+		}
 	}
 
 }
